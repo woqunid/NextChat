a211a50c2de4558169a9f6d2caae3384a98b0d6e
李鑫 <3424348553@qq.com>
2026-04-19 12:42:38 +0800

    增加适配性

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -2,31 +2,34 @@ import { NextResponse } from "next/server";
 
 import { getServerSideConfig } from "../../config/server";
 
-const serverConfig = getServerSideConfig();
-
 // Danger! Do not hard code any secret value here!
 // 警告！不要在这里写入任何敏感信息！
-const DANGER_CONFIG = {
-  needCode: serverConfig.needCode,
-  hideUserApiKey: serverConfig.hideUserApiKey,
-  disableGPT4: serverConfig.disableGPT4,
-  hideBalanceQuery: serverConfig.hideBalanceQuery,
-  disableFastLink: serverConfig.disableFastLink,
-  hasServerApiKey: serverConfig.hasServerApiKey,
-  customModels: serverConfig.customModels,
-  defaultModel: serverConfig.defaultModel,
-  visionModels: serverConfig.visionModels,
-};
+function getDangerConfig() {
+  const serverConfig = getServerSideConfig();
+
+  return {
+    needCode: serverConfig.needCode,
+    hideUserApiKey: serverConfig.hideUserApiKey,
+    disableGPT4: serverConfig.disableGPT4,
+    hideBalanceQuery: serverConfig.hideBalanceQuery,
+    disableFastLink: serverConfig.disableFastLink,
+    hasServerApiKey: serverConfig.hasServerApiKey,
+    customModels: serverConfig.customModels,
+    defaultModel: serverConfig.defaultModel,
+    visionModels: serverConfig.visionModels,
+  };
+}
 
 declare global {
-  type DangerConfig = typeof DANGER_CONFIG;
+  type DangerConfig = ReturnType<typeof getDangerConfig>;
 }
 
 async function handle() {
-  return NextResponse.json(DANGER_CONFIG);
+  return NextResponse.json(getDangerConfig());
 }
 
 export const GET = handle;
 export const POST = handle;
 
-export const runtime = "edge";
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
